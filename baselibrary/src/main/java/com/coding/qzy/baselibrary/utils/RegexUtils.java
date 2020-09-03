1eca8950eb3fbed3e15799f964a66509d5eecc33
quzongyang <quzongyang@xiaohe.com>
2020-09-03 10:06:14 +0800

    add

---
+++ baselibrary/src/main/java/com/coding/qzy/baselibrary/utils/RegexUtils.java
@@ -0,0 +1,165 @@
+package com.coding.qzy.baselibrary.utils;
+
+import android.text.TextUtils;
+
+import java.util.regex.Pattern;
+
+/**
+ * author : quzongyang
+ * e-mail : quzongyang@xiaohe.com
+ * time   : 2020/09/03
+ * desc   : 验证数据有效性管理工具类
+ * version: 1.0
+ */
+
+
+public class RegexUtils {
+    /**
+     * 正则表达式:验证用户名(不包含中文和特殊字符)如果用户名使用手机号码或邮箱 则结合手机号验证和邮箱验证
+     */
+    public static final String REGEX_USERNAME = "^[a-zA-Z]\\w{5,17}$";
+
+    /**
+     * 正则表达式:验证密码(不包含特殊字符)
+     */
+    public static final String REGEX_PASSWORD = "^[a-zA-Z0-9]{6,16}$";
+
+    /**
+     * 正则表达式:验证手机号
+     */
+    public static final String REGEX_MOBILE = "^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$";
+
+    /**
+     * 正则表达式:验证邮箱
+     */
+    public static final String REGEX_EMAIL = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+
+    /**
+     * 正则表达式:验证汉字(1-9个汉字)  {1,9} 自定义区间
+     */
+    public static final String REGEX_CHINESE = "^[\u4e00-\u9fa5]{1,9}$";
+
+    /**
+     * 正则表达式:验证身份证
+     */
+    public static final String REGEX_ID_CARD = "(^\\d{15}$)|(^\\d{17}([0-9]|X)$)";
+
+    /**
+     * 正则表达式:验证URL
+     */
+    public static final String REGEX_URL = "http(s)?://([\\w-]+\\.)+[\\w-]+(/[\\w- ./?%&=]*)?";
+
+    /**
+     * 正则表达式:验证IP地址
+     */
+    public static final String REGEX_IP_ADDR = "(2[5][0-5]|2[0-4]\\d|1\\d{2}|\\d{1,2})\\.(25[0-5]|2[0-4]\\d|1\\d{2}|\\d{1,2})\\.(25[0-5]|2[0-4]\\d|1\\d{2}|\\d{1,2})\\.(25[0-5]|2[0-4]\\d|1\\d{2}|\\d{1,2})";
+
+
+    /**
+     * 验证整数（正整数和负整数）
+     * @param digit 一位或多位0-9之间的整数
+     * @return 验证成功返回true，验证失败返回false
+     */
+    public static boolean checkDigit(String digit) {
+        String regex = "\\-?[1-9]\\d+";
+        return Pattern.matches(regex,digit);
+    }
+
+    /**
+     * 验证金额有效性
+     * @param price
+     * @return
+     */
+    public static boolean isPrice(String price) {
+        if(TextUtils.isEmpty(price.trim())){
+            return false;
+        }else{
+            //^(([1-9]{1}\d*)|([0]{1}))(\.(\d){1,2})?$       0/0.0/0.00 可以
+            //^(([1-9]\d*)(\.\d{1,2})?|0\.([1-9]|\d[1-9])|0)$   0.0/0.00 都不行
+            String regex = "^(([1-9]\\d*)(\\.\\d{1,2})?|0\\.([1-9]|\\d[1-9])|0)$";
+            return Pattern.matches(regex, price);
+        }
+    }
+
+
+    /**
+     * 验证整数和浮点数（正负整数和正负浮点数）
+     * @param decimals 一位或多位0-9之间的浮点数，如：1.23，233.30
+     * @return 验证成功返回true，验证失败返回false
+     */
+    public static boolean checkDecimals(String decimals) {
+        String regex = "\\-?[1-9]\\d+(\\.\\d+)?";
+        return Pattern.matches(regex,decimals);
+    }
+    /**
+     * 校验用户名
+     * @param username
+     * @return 校验通过返回true，否则返回false
+     */
+    public static boolean isUserName(String username) {
+        return Pattern.matches(REGEX_USERNAME, username);
+    }
+
+    /**
+     * 校验密码
+     * @param password
+     * @return 校验通过返回true，否则返回false
+     */
+    public static boolean isPassword(String password) {
+        return Pattern.matches(REGEX_PASSWORD, password);
+    }
+
+    /**
+     * 校验手机号
+     * @param mobile
+     * @return 校验通过返回true，否则返回false
+     */
+    public static boolean isMobile(String mobile) {
+        return Pattern.matches(REGEX_MOBILE, mobile);
+    }
+
+    /**
+     * 校验邮箱
+     * @param email
+     * @return 校验通过返回true，否则返回false
+     */
+    public static boolean isEmail(String email) {
+        return Pattern.matches(REGEX_EMAIL, email);
+    }
+
+    /**
+     * 校验汉字
+     * @param chinese
+     * @return 校验通过返回true，否则返回false
+     */
+    public static boolean isChinese(String chinese) {
+        return Pattern.matches(REGEX_CHINESE, chinese);
+    }
+
+    /**
+     * 校验身份证
+     * @param idCard
+     * @return 校验通过返回true，否则返回false
+     */
+    public static boolean isIDCard(String idCard) {
+        return Pattern.matches(REGEX_ID_CARD, idCard);
+    }
+
+    /**
+     * 校验URL
+     * @param url
+     * @return 校验通过返回true，否则返回false
+     */
+    public static boolean isUrl(String url) {
+        return Pattern.matches(REGEX_URL, url);
+    }
+
+    /**
+     * 校验IP地址
+     * @param ipAddress
+     * @return
+     */
+    public static boolean isIPAddress(String ipAddress) {
+        return Pattern.matches(REGEX_IP_ADDR, ipAddress);
+    }
+}
