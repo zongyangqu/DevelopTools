3b07ae3c626fe5baea1d4209da78c02f4b2221f5
quzongyang <quzongyang@xiaohe.com>
2020-12-08 17:26:09 +0800

    add

---
+++ baselibrary/src/main/java/com/coding/qzy/baselibrary/widget/SwitchButton.java
@@ -0,0 +1,13 @@
+package com.coding.qzy.baselibrary.widget;
+
+/**
+ * author : quzongyang
+ * e-mail : quzongyang@xiaohe.com
+ * time   : 2020/12/08
+ * desc   :
+ * version: 1.0
+ */
+
+
+public class SwitchButton {
+}
