2d003f0076b3de6f0017ace34ef586b88442701a
quzongyang <quzongyang@xiaohe.com>
2021-03-24 18:06:12 +0800

    添加二维码

--- app/src/main/java/com/quzy/coding/ui/activity/QRCodeActivity.java
+++ app/src/main/java/com/quzy/coding/ui/activity/QRCodeActivity.java
@@ -1,44 +1,28 @@
 package com.quzy.coding.ui.activity;
 
-import android.app.Application;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.media.AudioFormat;
-import android.os.Environment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.quzy.coding.BuildConfig;
+import com.coding.qzy.baselibrary.zxlib.CaptureActivity;
+import com.coding.qzy.baselibrary.zxlib.util.QrCodeGenerator;
 import com.quzy.coding.R;
 import com.quzy.coding.base.BaseActivity;
-import com.quzy.coding.base.BaseApplication;
-import com.quzy.coding.util.Singleton;
-import com.quzy.coding.util.TestManager;
-import com.quzy.coding.util.UZXing;
-import com.quzy.coding.util.view.AudioView;
-import com.zlw.main.recorderlib.RecordManager;
-import com.zlw.main.recorderlib.recorder.RecordConfig;
-import com.zlw.main.recorderlib.recorder.RecordHelper;
-import com.zlw.main.recorderlib.recorder.listener.RecordFftDataListener;
-import com.zlw.main.recorderlib.recorder.listener.RecordResultListener;
-import com.zlw.main.recorderlib.recorder.listener.RecordSoundSizeListener;
-import com.zlw.main.recorderlib.recorder.listener.RecordStateListener;
 
-import java.io.File;
+import com.quzy.coding.util.Constants;
 import java.lang.ref.ReferenceQueue;
-import java.lang.ref.SoftReference;
-import java.lang.ref.WeakReference;
-import java.util.Locale;
 
 import butterknife.BindView;
-import butterknife.OnClick;
 
 /**
  * author : quzongyang
@@ -49,23 +33,135 @@ import butterknife.OnClick;
  */
 
 
-public class QRCodeActivity extends BaseActivity {
+public class QRCodeActivity extends BaseActivity implements View.OnClickListener{
 
 
-    @BindView(R.id.ivQrcode)
-    ImageView ivQrcode;
+    @BindView(R.id.btn_qrcode)
+    Button btnQrCode; // 扫码
+    @BindView(R.id.txt_result)
+    TextView tvResult; // 结果
+    @BindView(R.id.btn_generate)
+    Button btnGenerate; // 生成二维码
+    @BindView(R.id.et_content)
+    EditText etContent; // 待生成内容
+    @BindView(R.id.img_qrcode)
+    ImageView imgQrcode; // 二维码图片
 
     private static ReferenceQueue<Object> rq = new ReferenceQueue<Object>();
     @Override
     protected void onViewCreated() {
-        Bitmap qrCode = UZXing.createQRImage("https://www.baidu.com/",200,200, Color.parseColor("#ffffffff"),Color.parseColor("#333333"));
-        ivQrcode.setImageBitmap(qrCode);
-        TestManager manager = TestManager.getInstance(this);
+//        Bitmap qrCode = UZXing.createQRImage("https://www.baidu.com/",200,200, Color.parseColor("#ffffffff"),Color.parseColor("#333333"));
+//        ivQrcode.setImageBitmap(qrCode);
+//        TestManager manager = TestManager.getInstance(this);
+        initView();
     }
 
     @Override
     protected int getLayoutId() {
         return R.layout.activity_layout_qrcode;
+    }
+
+
+    private void initView() {
+        btnQrCode.setOnClickListener(this);
+        btnGenerate.setOnClickListener(this);
+    }
+
+    // 开始扫码
+    private void startQrCode() {
+        // 申请相机权限
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // 申请权限
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission
+                    .CAMERA)) {
+                Toast.makeText(this, "请至权限中心打开本应用的相机访问权限", Toast.LENGTH_SHORT).show();
+            }
+            ActivityCompat.requestPermissions(QRCodeActivity.this, new String[]{Manifest.permission.CAMERA}, Constants.REQ_PERM_CAMERA);
+            return;
+        }
+        // 申请文件读写权限（部分朋友遇到相册选图需要读写权限的情况，这里一并写一下）
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // 申请权限
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission
+                    .WRITE_EXTERNAL_STORAGE)) {
+                Toast.makeText(this, "请至权限中心打开本应用的文件读写权限", Toast.LENGTH_SHORT).show();
+            }
+            ActivityCompat.requestPermissions(QRCodeActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, Constants.REQ_PERM_EXTERNAL_STORAGE);
+            return;
+        }
+        // 二维码扫码
+        Intent intent = new Intent(QRCodeActivity.this, CaptureActivity.class);
+        startActivityForResult(intent, Constants.REQ_QR_CODE);
+    }
+
+    /**
+     * 生成二维码
+     */
+    private void generateQrCode() {
+        if (etContent.getText().toString().equals("")) {
+            Toast.makeText(this, "请输入二维码内容", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Bitmap bitmap = QrCodeGenerator.getQrCodeImage(etContent.getText().toString(), imgQrcode.getWidth(), imgQrcode.getHeight());
+        if (bitmap == null) {
+            Toast.makeText(this, "生成二维码出错", Toast.LENGTH_SHORT).show();
+            imgQrcode.setImageBitmap(null);
+        } else {
+            imgQrcode.setImageBitmap(bitmap);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_qrcode:
+                startQrCode();
+                break;
+            case R.id.btn_generate:
+                generateQrCode();
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //扫描结果回调
+        if (requestCode == Constants.REQ_QR_CODE && resultCode == RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            String scanResult = bundle.getString(Constants.INTENT_EXTRA_KEY_QR_SCAN);
+            //将扫描出的信息显示出来
+            tvResult.setText(scanResult);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case Constants.REQ_PERM_CAMERA:
+                // 摄像头权限申请
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 获得授权
+                    startQrCode();
+                } else {
+                    // 被禁止授权
+                    Toast.makeText(QRCodeActivity.this, "请至权限中心打开本应用的相机访问权限", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case Constants.REQ_PERM_EXTERNAL_STORAGE:
+                // 文件读写权限申请
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 获得授权
+                    startQrCode();
+                } else {
+                    // 被禁止授权
+                    Toast.makeText(QRCodeActivity.this, "请至权限中心打开本应用的文件读写权限", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
     }
 
 
--- app/src/main/java/com/quzy/coding/util/Constants.java
+++ app/src/main/java/com/quzy/coding/util/Constants.java
@@ -25,4 +25,11 @@ public class Constants {
     public final static int  CustomControl = 11;
     public final static int  ExternalResourceLoad = 12;
     public final static int  HotFix = 13;
+
+
+    // request参数
+    public static final int REQ_QR_CODE = 11002; // // 打开扫描界面请求码
+    public static final int REQ_PERM_CAMERA = 11003; // 打开摄像头
+    public static final int REQ_PERM_EXTERNAL_STORAGE = 11004; // 读写文件
+    public static final String INTENT_EXTRA_KEY_QR_SCAN = "qr_scan_result";
 }
